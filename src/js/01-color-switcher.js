f80408e5fa225cfec78649f3ddc850753fc81b22
Uliana Komarynska <uliakom@gmail.com>
2022-05-13 23:43:38 +0300

    Task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+const refs = {
+    backgroundColor: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]')
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  timeoutId = setInterval(changeBackgroundColor, 1000);
+  refs.startBtn.setAttribute('disabled', '');
+  refs.stopBtn.toggleAttribute('disabled', '');
+}
+
+function changeBackgroundColor() {
+  refs.backgroundColor.style.backgroundColor = getRandomHexColor();
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function onStopBtnClick() {
+  refs.stopBtn.setAttribute('disabled', '');
+  refs.startBtn.toggleAttribute('disabled', '');
+  clearInterval(timeoutId);
+}
